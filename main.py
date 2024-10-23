c07bcbb44a39c8d88fec1a220c99ed79f4a6b29a
andrhars <andreas@haarsaker.net>
2024-10-23 08:55:35 +0200

    Changed screen draw method to .flip(), added FPS limiter

--- main.py
+++ main.py
@@ -9,16 +9,23 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    a = 1
-    while a == 1:
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        pygame.display.update()
+
+        screen.fill("black")
+        pygame.display.flip()
+
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
